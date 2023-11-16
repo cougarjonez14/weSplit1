93a6f79caac8d03449dcfb953566596a7289ef15
stationAstral <cougarjonez@gmail.com>
2023-11-16 13:39:40 -0800

    Initial Commit

---
+++ weSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weSplit
+//
+//  Created by stationAstral on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
